4518350c2e54acb2239bc0a4c9439f9b3acf0eda
Maxime Charles <maxfoow@gmail.com>
2017-04-10 16:29:04 +0200

    Add configuration extensions for AppInsights

--- src/Orion.Extensions.Logging.AppInsights/LoggerFactoryExtensions.cs
+++ src/Orion.Extensions.Logging.AppInsights/LoggerFactoryExtensions.cs
@@ -3,14 +3,25 @@ using Microsoft.Extensions.Logging;
 
 namespace Orion.Extensions.Logging.AppInsights {
     public static class LoggerFactoryExtensions {
-        public static void AddAppInsights(this ILoggerFactory loggerFactory, string instrumentationKey,
+        public static ILoggerFactory AddAppInsights(this ILoggerFactory loggerFactory, string instrumentationKey,
             bool developerMode = false) {
+            return loggerFactory.AddAppInsights((category, logLevel) => logLevel >= LogLevel.Information, instrumentationKey, developerMode);
+        }
+
+        public static ILoggerFactory AddAppInsights(this ILoggerFactory loggerFactory, LogLevel minLevel, string instrumentationKey, bool developerMode = false) {
+            return loggerFactory.AddAppInsights((category, logLevel) => logLevel >= minLevel, instrumentationKey, developerMode);
+        }
+
+        public static ILoggerFactory AddAppInsights(this ILoggerFactory loggerFactory, Func<string, LogLevel, bool> filter, string instrumentationKey, bool developerMode = false)
+        {
             if (string.IsNullOrEmpty(instrumentationKey))
                 throw new ArgumentException(nameof(instrumentationKey));
-            loggerFactory.AddProvider(new LoggerProvider(null, new Settings {
+            loggerFactory.AddProvider(new LoggerProvider(filter, new Settings
+            {
                 InstrumentationKey = instrumentationKey,
                 DeveloperMode = developerMode
             }));
+            return loggerFactory;
         }
     }
 }
